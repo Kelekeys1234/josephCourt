fc83968ed42ade1153458dcd0dd3918594d5ac40
kelekeys1234 <fkelenna@gmail.com>
2022-11-14 12:51:12 -0800

    First commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(function($, document, window){
+	
+	$(document).ready(function(){
+
+		// Cloning main navigation for mobile menu
+		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+		// Mobile menu toggle 
+		$(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
+
+		$(".hero").flexslider({
+			directionNav: false,
+			controlNav: true
+		});
+	});
+
+	$(window).load(function(){
+
+	});
+
+})(jQuery, document, window);
+function initializeMap() {
+	console.log('initialize');
+	var myOptions = {
+	  center: new google.maps.LatLng(-34.397, 150.644),
+	  zoom: 8,
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
+	  myOptions);
+  }
+  
---
+++ my-appnpx/src/component/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function Header(){
+  return(
+      <div>
+          	<div class="welcome">
+              <nav class="navbar navbar-expand-lg fixed-top" > 
+              <div class="container-fluid">
+						
+              <a class="navbar-brand" ><img src ="images/Rccg.png" /></a>
+						
+					
+					
+                    </div>
+                    </nav>
+              </div>
+
+      </div>
+  )
+}
+export default Header;
